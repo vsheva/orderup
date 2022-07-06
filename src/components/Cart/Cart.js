f8369a42a2f6d14469f303423119edfafdcb6ef3
vsheva <valera.sheva@gmail.com>
2022-07-06 20:39:13 +0300

    complex cart reducer realization for items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem'
+import CartContext from '../../store/cart-context.js'
+
 
 const Cart = props => {
+    const cartCtx = useContext(CartContext)
 
-  const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Pizza', amount: 2, price: 9.99 }].map(item => (
-        <li>{item.name}</li>
-      ))}
-    </ul>
-  ); //<ul>{ }</ul>  - map -> li
+    const totalAmount= `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0
+
+    const cartItemRemoveHandler =(id)=>{}
+    const cartItemAddHandler =(item)=>{}
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map(item => (
+                <CartItem key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onRemove={()=>{cartItemRemoveHandler(item.id)}}
+                          onAdd={()=>{cartItemAddHandler( item)}}
+                />
+            ))}
+        </ul>
+    );
 
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
-    </Modal>
-  );
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+                {hasItems && <button className={classes.button}>Order</button>}
+            </div>
+        </Modal>
+    );
 };
 
 export default Cart;
 
-/*const cartItems = (
-  <ul className={classes['cart-items']}>
-    {[{ id: 'c1', name: 'Pizza', amount: 2, price: 9.99 }].map(item => (
-      <li>{item.name}</li>
-    ))}
-  </ul>
-);*/
-//const cartItems =<ul className = {classes["cart-items"]}>{[{id:"1", name:"Pizza", price:12.99, amount:2}].map(item=>{<li>{item.name}</li>}) }</ul>
+
+
+//onRemove={cartItemRemoveHandler.bind(null, item.id)}
+//                           onAdd={cartItemAddHandler.bind(null, item)}
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,19 +2,34 @@ import CartContext from './cart-context';
 import React, { useReducer } from 'react';
 
 const defaultCartState = {
-  items: [],
+  items: [],       //state.items
   totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
-                                                                                        //соединяем cтейтовый массив пустов + actionовский item = в новый массив , т.е. в массив добавили item
+
     const updatedAmount = state.totalAmount + action.item.amount * action.item.price; //соединяем стейт (state.totalAmount из дефолта) и action(action.item, которое у нас {}//тот item из action будет обьектом, со свойствами item.name и item.price
 
-    console.log("action.item.amount", +action.item.amount) //
-    console.log("state.totalAmount", state.totalAmount )   //
-    console.log("action.item.price", action.item.price)    //
+    // ВАЖНАЯ СЛОЖНАЯ ЛОГИКА ДОБАВЛЕНИЯ В КОРЗИНУ !!!
+    const existingCartItemIndex = state.items.findIndex(item => item.id===action.item.id) //в массиве проверяем по Id (если товар к-ё мы ищем равен товару, который был диспатчен, те. отправлен
+    const existingCartItem = state.items[existingCartItemIndex] //когда нашли наш товор в массиве(корзине)  // если не нашли, то этой строки не будет
+
+    let updatedItem //*
+    let updatedItems //корзина
+
+    //если такой товар уже есть в корзине
+    if (existingCartItem) {
+      updatedItem= {...existingCartItem, amount: existingCartItem.amount+action.item.amount } //обновленная позиция товара с новым количеством
+      updatedItems = [...state.items] //копируем массив с со старым содержимым
+      updatedItems[existingCartItemIndex] =updatedItem // на место позиции пиццы в массиве-  перезаписываем новое значение пиццы тем количеством, которое было вычеслено выше
+    }
+    //если товар доавляется в корзину впервые
+    else {
+        //updatedItem ={...action.item}   //наш action, который мы добавляе в state массив на вход ниже в state.items.concat(updatedItem)
+      updatedItems = state.items.concat({...action.item});//соединяем cтейтовый массив пустов + actionовский item = в новый массив , т.е. в массив добавили item
+    }
+
 
     return {
       items: updatedItems,
@@ -23,6 +38,9 @@ const cartReducer = (state, action) => {
   }
   return defaultCartState;
 };
+
+
+
 
 const CartProvider = props => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
